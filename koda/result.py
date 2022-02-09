0d0fc1e49a0c147a600456ce7555f226b2ce6999
Keith Philpott <keith.philpott@graphitehq.com>
2022-02-08 23:02:29 -0800

    move tests out of docstrings

--- koda/result.py
+++ koda/result.py
@@ -8,39 +8,25 @@ from koda._generics import A, FailT, B
 class Ok(Generic[A]):
     val: A
 
-    def apply(self, container: Result[Callable[[A], B], FailT]) -> Result[B, FailT]:
+    def apply(self, container: "Result[Callable[[A], B], FailT]") -> "Result[B, FailT]":
         if isinstance(container, Ok):
             return Ok(container.val(self.val))
         else:
             return container
 
-    def flat_map(self, fn: Callable[[A], Result[B, FailT]]) -> Result[B, FailT]:
+    def flat_map(self, fn: Callable[[A], "Result[B, FailT]"]) -> "Result[B, FailT]":
         return fn(self.val)
 
-    def flat_map_err(self, fn: Callable[[Any], Result[A, Any]]) -> "Ok[A]":
-        """
-        >>> def add_one(val: int) -> Result[int, Any]: return Err(val + 1)
-        >>> Ok(5).flat_map_err(add_one)
-        Ok(val=5)
-        """
+    def flat_map_err(self, fn: Callable[[Any], "Result[A, Any]"]) -> "Ok[A]":
         return self
 
     def map(self, fn: Callable[[A], B]) -> "Ok[B]":
         return Ok(fn(self.val))
 
-    def map_err(self, fn: Callable[[Any], Result[Any, Any]]) -> "Ok[A]":
-        """
-        >>> def add_one(val: int) -> Result[int, Any]: return Err(val + 1)
-        >>> Ok(5).map_err(add_one)
-        Ok(val=5)
-        """
+    def map_err(self, fn: Callable[[Any], "Any"]) -> "Ok[A]":
         return self
 
     def swap(self) -> "Err[A]":
-        """
-        >>> Ok(5).swap()
-        Err(val=5)
-        """
         return Err(self.val)
 
 
@@ -48,42 +34,24 @@ class Ok(Generic[A]):
 class Err(Generic[FailT]):
     val: FailT
 
-    def apply(self, _: Result[Callable[[Any], Any], FailT]) -> "Err[FailT]":
+    def apply(self, _: "Result[Callable[[Any], Any], FailT]") -> "Err[FailT]":
         return self
 
     def map(self, _: Callable[[Any], Any]) -> "Err[FailT]":
-        """
-        >>> Err(3).map(lambda _: 25)
-        Err(val=3)
-        """
         return self
 
-    def flat_map(self, _: Callable[[Any], Result[Any, Any]]) -> "Err[FailT]":
+    def flat_map(self, _: Callable[[Any], "Result[Any, Any]"]) -> "Err[FailT]":
         return self
 
     def flat_map_err(
-        self, fn: Callable[[FailT], Result[A, FailT]]
-    ) -> Result[A, FailT]:
-        """
-        >>> def add_one(val: int) -> Result[int, Any]: return Err(val + 1)
-        >>> Err(5).flat_map_err(add_one)
-        Err(val=6)
-        """
+        self, fn: Callable[[FailT], "Result[A, B]"]
+    ) -> "Result[A, B]":
         return fn(self.val)
 
     def map_err(self, fn: Callable[[FailT], B]) -> "Err[B]":
-        """
-        >>> def _int_to_str(n: int) -> str: return str(n)
-        >>> Err(5).map_err(_int_to_str)
-        Err(val='5')
-        """
         return Err(fn(self.val))
 
     def swap(self) -> Ok[FailT]:
-        """
-        >>> Err(3).swap()
-        Ok(val=3)
-        """
         return Ok(self.val)
 
 
--- tests/test_result.py
+++ tests/test_result.py
@@ -1,5 +1,7 @@
-from koda.result import Err, Ok
-from tests.utils import enforce_functor_one_val, enforce_monad_unit, enforce_monad_flat_map, enforce_applicative_apply
+from koda.result import Err, Ok, Result
+from tests.utils import enforce_functor_one_val, enforce_monad_unit, \
+    enforce_monad_flat_map, enforce_applicative_apply
+from typing import Any
 
 
 def test_result() -> None:
@@ -7,3 +9,43 @@ def test_result() -> None:
     enforce_monad_unit(Ok)
     enforce_monad_flat_map(Ok, Err("something went wrong"))
     enforce_applicative_apply(Ok, Err("something went wrong"))
+
+
+def test_ok_flat_map_err() -> None:
+    def add_one(val: int) -> Result[int, Any]:
+        return Err(val + 1)
+
+    assert Ok(5).flat_map_err(add_one) == Ok(5)
+
+
+def test_ok_map_err() -> None:
+    def str_twice(val: str) -> str:
+        return f"{val}{val}"
+
+    assert Ok(5).map_err(str_twice) == Ok(5)
+
+
+def test_ok_swap() -> None:
+    assert Ok(5).swap() == Err(5)
+
+
+def test_err_map() -> None:
+    assert Err(3).map(lambda _: 25) == Err(3)
+
+
+def test_err_flat_map_err() -> None:
+    def add_one(val: int) -> Result[str, int]:
+        return Err(val + 1)
+
+    assert Err(5).flat_map_err(add_one) == Err(6)
+
+
+def test_err_map_err() -> None:
+    def _int_to_str(n: int) -> str:
+        return str(n)
+
+    assert Err(5).map_err(_int_to_str) == Err('5')
+
+
+def test_err_swap() -> None:
+    assert Err(3).swap() == Ok(3)
